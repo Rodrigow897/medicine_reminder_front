4c9df6d1048ee23d1c37b1bc0659ff41f0908674
Rodrigo <rwesley2000@gmail.com>
2025-11-13 18:48:12 -0300

    style: icon

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,11 +1,15 @@
 import '@/global.css';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import { Modal, Text, View } from "react-native";
 
 export default function ModalLogin(){
     return (
         <Modal transparent animationType="fade" visible={true}>
             {/* Fundo semitransparente para escurecer o resto da tela */}
-            <View className="flex-1 flex-col justify-end items-center bg-[#C02636]">
+            <View className="flex-1 flex-col justify-end items-center gap-20 bg-[#C02636]">
+                <View className='mb-12'>
+                    <MaterialCommunityIcons name="heart-plus-outline" size={24} color="white" />
+                </View>
                 {/* Conteúdo do modal — metade da altura da tela */}
                 <View className="w-full h-1/2 bg-white rounded-t-3xl p-4 items-center">
                     <View className='w-[85%] pt-8 pb-8'>
