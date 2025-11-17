98b2524e6e32ad2eba8db9f4f958f3e76df18e50
Rodrigo <rwesley2000@gmail.com>
2025-11-17 17:57:48 -0300

    feat: switch and some style to addprescription

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -9,7 +9,7 @@ export default function Input({placeholder}: props){
     return(
         <TextInput className="h-[58px] pl-[10px] pr-[10px] border-[1px] rounded-2xl border-[#A2B9CD] bg-white text-[16px] text-[#17222B]"
             placeholder={placeholder}
-            placeholderTextColor="#17222B"
+            placeholderTextColor="#4D708F"
         />
     )
 }
