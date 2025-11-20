724cfd8ba1a18521a75c2ba61dcf839652a59216
Rodrigo <rwesley2000@gmail.com>
2025-11-20 18:12:31 -0300

    style: style to input select

--- src/components/InputSelect.tsx
+++ src/components/InputSelect.tsx
@@ -9,14 +9,18 @@ type Item = {
 };
 
 const data: Item[] = [
-  { label: "Item 1", value: "1" },
-  { label: "Item 2", value: "2" },
-  { label: "Item 3", value: "3" },
-  { label: "Item 4", value: "4" },
-  { label: "Item 5", value: "5" },
-  { label: "Item 6", value: "6" },
-  { label: "Item 7", value: "7" },
-  { label: "Item 8", value: "8" },
+  { label: "A cada 3 horas", value: "1" },
+  { label: "A cada 4 horas", value: "2" },
+  { label: "A cada 6 horas", value: "3" },
+  { label: "A cada 12 horas", value: "4" },
+  { label: "A cada 1 dia", value: "5" },
+  { label: "A cada 2 dias", value: "6" },
+  { label: "A cada 3 dias", value: "7" },
+  { label: "A cada 4 dias", value: "8" },
+  { label: "A cada 5 dias", value: "9" },
+  { label: "A cada 6 dias", value: "10" },
+  { label: "A cada 7 dias", value: "11" },
+  { label: "A cada 8 dias", value: "12" },
 ];
 
 const DropdownComponent: React.FC = () => {
@@ -27,7 +31,7 @@ const DropdownComponent: React.FC = () => {
     if (value || isFocus) {
       return (
         <Text
-          className={`absolute left-[22px] top-2 z-50 px-2 bg-white text-[14px] ${
+          className={`absolute left-[22px] top-[-8] z-50 px-2 bg-white text-[14px] ${
             isFocus ? "text-blue-600" : "text-black"
           }`}
         >
@@ -50,16 +54,16 @@ const DropdownComponent: React.FC = () => {
           paddingHorizontal: 8,
           borderColor: isFocus ? "blue" : "gray",
         }}
-        placeholderStyle={{ fontSize: 16 }}
+        placeholderStyle={{ fontSize: 16, }}
         selectedTextStyle={{ fontSize: 16 }}
-        inputSearchStyle={{ fontSize: 16, height: 40 }}
+        inputSearchStyle={{ fontSize: 16, color:'#9ca3af', height: 40 }}
         iconStyle={{ width: 20, height: 20 }}
         data={data}
         search
         maxHeight={175}
         labelField="label"
         valueField="value"
-        placeholder={!isFocus ? "Select item" : "..."}
+        placeholder={!isFocus ? "Selecione" : "..."}
         searchPlaceholder="Search..."
         value={value}
         onFocus={() => setIsFocus(true)}
